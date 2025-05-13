a7d74d7510467aa8b8feec36e37e3ab5d8add094
botkomakers <siam420riiya@gmail.com>
2025-05-13 10:51:13 +0600

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -1,6 +1,1 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-@Client.on_message(filters.command("start") & filters.private)
-async def start_handler(client: Client, message: Message):
-    await message.reply_text("Hello! I am alive and ready to download your videos.")
+#থ
