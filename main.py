8bbfa6315a9525ad2857e4a96c92dec50a8e17e7
botkomakers <siam420riiya@gmail.com>
2025-05-13 09:23:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ class Bot(Client):
             workers=150,
             sleep_threshold=10
         )
-      
-    async def start(self):           
+
+    async def start(self):
         await super().start()
         me = await self.get_me()
-        self.username = '@' + me.username          
-        print('Bot Started Powered By @VJ_Botz')
+        self.username = '@' + me.username
+        print(f"Bot Started Successfully as {self.username} | Powered By @VJ_Botz")
 
     async def stop(self, *args):
         await super().stop()
-        print('Bot Stopped Bye')
+        print("Bot Stopped. Bye!")
 
 Bot().run()
