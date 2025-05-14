1634720a29a869bef9b1bd00695138e8f2c2f9f9
botkomakers <siam420riiya@gmail.com>
2025-05-14 10:08:13 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,8 +1,23 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-@Client.on_message(filters.command("start") & filters.private)
-async def start_handler(client, message: Message):
+@Client.on_message(filters.private & filters.command("start"))
+async def start_command(client: Client, message: Message):
     await message.reply_text(
-        "Hello! Send me any video link from YouTube, Facebook, TikTok, etc. and I’ll download it for you!"
+        text=(
+            "**Welcome!**\n\n"
+            "I can download videos from **direct video links**.\n"
+            "Just send me any direct video link like:\n"
+            "`https://example.com/video.mp4`\n\n"
+            "**Features:**\n"
+            "- Supports multiple links\n"
+            "- Fast CDN downloading\n"
+            "- Automatic upload to Telegram"
+        ),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("Support", url="https://t.me/your_support_group")],
+                [InlineKeyboardButton("Updates", url="https://t.me/your_update_channel")]
+            ]
+        )
     )
