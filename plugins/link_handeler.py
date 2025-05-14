f6190515f62446f31a3cdb2aa3f848f2ec4eec4b
botkomakers <siam420riiya@gmail.com>
2025-05-14 09:20:57 +0600

    Update link_handeler.py

--- plugins/link_handeler.py
+++ plugins/link_handeler.py
@@ -12,23 +12,22 @@ async def direct_video_handler(bot: Client, message: Message):
     if not url.lower().startswith("http"):
         return
 
-    msg = await message.reply_text("Checking the link...")
+    msg = await message.reply_text("Validating the video link...")
 
     try:
-        head = requests.head(url, allow_redirects=True)
-        content_type = head.headers.get("content-type", "")
+        response = requests.get(url, stream=True)
+        content_type = response.headers.get("content-type", "")
 
         if not content_type.startswith("video/"):
-            return await msg.edit("This link does not point to a valid video file.")
+            return await msg.edit("This link does not seem to be serving a valid video file.")
 
         ext = mimetypes.guess_extension(content_type.split(";")[0]) or ".mp4"
         filename = "video" + ext
 
         await msg.edit("Downloading the video...")
 
-        r = requests.get(url, stream=True)
         with open(filename, "wb") as f:
-            for chunk in r.iter_content(chunk_size=1024 * 1024):
+            for chunk in response.iter_content(chunk_size=1024 * 1024):
                 if chunk:
                     f.write(chunk)
 
