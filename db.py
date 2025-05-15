04d4fa64fb07749d55b49662ff6976aaad6f691a
botkomakers <siam420riiya@gmail.com>
2025-05-15 10:36:44 +0600

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,20 @@
-#থড
+from pymongo import MongoClient
+from config import MONGO_DB_URI
+
+client = MongoClient(MONGO_DB_URI)
+db = client["video_downloader"]
+users = db["users"]
+
+def save_user(user_id, name, username):
+    users.update_one(
+        {"_id": user_id},
+        {"$set": {"name": name, "username": username}},
+        upsert=True
+    )
+
+def has_been_notified(user_id):
+    user = users.find_one({"_id": user_id})
+    return user and user.get("notified", False)
+
+def set_notified(user_id):
+    users.update_one({"_id": user_id}, {"$set": {"notified": True}})
