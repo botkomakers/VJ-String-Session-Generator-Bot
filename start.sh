effe69ef8d8334f36f36e49fe6515e6e3f53b4b3
botkomakers <siam420riiya@gmail.com>
2025-05-15 09:25:18 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,28 @@
-#!/bin/bash
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
 
-mkdir -p bin
-curl -L -o bin/ffmpeg https://ffmpeg-binaries.vercel.app/ffmpeg
-chmod +x bin/ffmpeg
-export PATH=$PATH:$(pwd)/bin
+class Bot(Client):
+def init(self):
+super().init(
+"universal_video_bot",
+api_id=API_ID,
+api_hash=API_HASH,
+bot_token=BOT_TOKEN,
+plugins=dict(root="plugins"),
+workers=150,
+sleep_threshold=10
+)
 
-python3 main.py
+async def start(self):  
+    await super().start()  
+    me = await self.get_me()  
+    print(f"Bot Started as @{me.username}")  
+
+async def stop(self, *args):  
+    await super().stop()  
+    print("Bot Stopped.")
+
+Bot().run()
+
+Thik acha
+
