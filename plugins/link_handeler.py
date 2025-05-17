32d76deb2902914ae1e3a1edc481caacf4d10292
botkomakers <siam420riiya@gmail.com>
2025-05-17 11:04:16 +0600

    Update link_handeler.py

--- plugins/link_handeler.py
+++ plugins/link_handeler.py
@@ -18,22 +18,11 @@ def download_with_ytdlp(url, download_dir="/tmp"):
         "format": "best[ext=mp4]/best",
         "quiet": True,
         "no_warnings": True,
-        "noplaylist": True,
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
         filename = ydl.prepare_filename(info)
         return filename, info
-
-def download_mega_file(url, download_dir="/tmp"):
-    from mega import Mega
-    mega = Mega()
-    m = mega.login()  # Anonymous login
-    file = m.download_url(url, dest_path=download_dir)
-    return file.name, {
-        "title": file.name,
-        "ext": os.path.splitext(file.name)[1].lstrip(".")
-    }
 
 def format_bytes(size):
     power = 1024
@@ -68,20 +57,6 @@ async def auto_cleanup(path="/tmp", max_age=300):
                 except:
                     pass
 
-def is_google_drive_link(url):
-    return "drive.google.com" in url
-
-def fix_google_drive_url(url):
-    if "uc?id=" in url or "export=download" in url:
-        return url
-    if "/file/d/" in url:
-        file_id = url.split("/file/d/")[1].split("/")[0]
-        return f"https://drive.google.com/uc?id={file_id}&export=download"
-    return url
-
-def is_mega_link(url):
-    return "mega.nz" in url or "mega.co.nz" in url
-
 @Client.on_message(filters.private & filters.text & ~filters.command(["start"]))
 async def auto_download_handler(bot: Client, message: Message):
     urls = message.text.strip().split()
@@ -100,17 +75,9 @@ async def auto_download_handler(bot: Client, message: Message):
     for url in valid_urls:
         filepath = None
         try:
-            if is_google_drive_link(url):
-                url = fix_google_drive_url(url)
-
             await notice.delete()
             processing = await message.reply_text(f"Downloading from:\n{url}")
-
-            if is_mega_link(url):
-                filepath, info = await asyncio.to_thread(download_mega_file, url)
-                filepath = os.path.join("/tmp", filepath)
-            else:
-                filepath, info = await asyncio.to_thread(download_with_ytdlp, url)
+            filepath, info = await asyncio.to_thread(download_with_ytdlp, url)
 
             if not os.path.exists(filepath):
                 raise Exception("Download failed or file not found.")
@@ -136,6 +103,7 @@ async def auto_download_handler(bot: Client, message: Message):
 
             await uploading.delete()
 
+            # Log to admin channel
             user = message.from_user
             file_size = format_bytes(os.path.getsize(filepath))
             log_text = (
